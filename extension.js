9747fc55371b5bfb024538e551593f8617796b34
shimeng <meng.shi@beebank.com>
2019-09-18 16:18:59 +0800

    0.07

---
+++ extension.js
@@ -0,0 +1,106 @@
+const vscode = require('vscode');
+const fs = require('fs');
+const webAppPublishPath = vscode.workspace.getConfiguration('writeVersion').get('webAppPublishPath');
+const needVersion = vscode.workspace.getConfiguration('writeVersion').get('needVersion');
+const needPageVers = vscode.workspace.getConfiguration('writeVersion').get('needPageVers');
+
+function dateFormat(fmt, date) {
+    let ret;
+    let opt = {
+        "Y+": date.getFullYear().toString(),        // 年
+        "m+": (date.getMonth() + 1).toString(),     // 月
+        "d+": date.getDate().toString(),            // 日
+        "H+": date.getHours().toString(),           // 时
+        "M+": date.getMinutes().toString(),         // 分
+        "S+": date.getSeconds().toString()          // 秒
+        // 有其他格式化字符需求可以继续添加，必须转化成字符串
+    };
+    for (let k in opt) {
+        ret = new RegExp("(" + k + ")").exec(fmt);
+        if (ret) {
+            fmt = fmt.replace(ret[1], (ret[1].length == 1) ? (opt[k]) : (opt[k].padStart(ret[1].length, "0")))
+        };
+    };
+    return fmt;
+}
+function writeHtmlVersion(filePath, suffix, fileName) {
+	if (!needVersion && !needPageVers) {
+		return;
+	}
+	try {
+		let file = fs.readFileSync(filePath).toString();
+		if (needVersion) {
+			var t = new RegExp('(' + fileName + ').(' + suffix + ')(\\??.*)(\"|\')');
+			file = file.replace(t, '$1.$2?v=' + new Date().getTime() + '$4');
+		}
+		if (needPageVers) {
+			file = file.replace(/(data-page-vers)="(\d+)"/, '$1="' + dateFormat('YYYYmmddHHMM', new Date()) + '"');
+		}
+		
+		const bf = Buffer.from(file);
+		fs.writeFileSync(filePath, bf);
+		if (webAppPublishPath)
+			fs.writeFileSync(getTargetPath(filePath), bf);
+	} catch(err) {
+		console.log(err)
+	}
+}
+function copyFile(sourcePath) {
+	if (!webAppPublishPath) return;
+	var readStream = fs.createReadStream(sourcePath);
+	var writeStream = fs.createWriteStream(getTargetPath(sourcePath));
+	readStream.pipe(writeStream);
+}
+function getTargetPath(sourcePath) {
+	return sourcePath.replace(/.+\\webapp\\(\w+)/, webAppPublishPath + '$1');
+}
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	let disposable = vscode.commands.registerCommand('extension.writeVersion', function () {
+		let includes = vscode.workspace.getConfiguration('writeVersion').get('includes');
+		includes = includes && includes.length ? includes : ['\\page\\'];
+		
+    	const filePath = vscode.window.activeTextEditor.document.uri.fsPath;
+    
+		const suffix = filePath.substr(filePath.lastIndexOf('.') + 1);
+		const fileName = filePath.substring(filePath.lastIndexOf('\\') + 1, filePath.lastIndexOf('.'));
+		if (includes && includes.length && ['css', 'js'].includes(suffix)) {
+			let isInclude = false
+			for (let i=0; i<includes.length; i++) {
+				if (~filePath.indexOf(includes[i])) {
+					isInclude = true;
+					break;
+				}
+			}
+			if (!isInclude) {
+				vscode.commands.executeCommand('workbench.action.files.save').then(function() {
+					copyFile(filePath);
+				});;
+				return false;
+			}
+		}
+		
+		if (suffix === 'js') {
+			writeHtmlVersion(filePath.replace('.js', '.html'), 'js', 'ytfw');
+		} else if (suffix === 'css') {
+			writeHtmlVersion(filePath.replace(/\\css\\(\w+).css/, '\\$1.html'), 'css', fileName);
+		}
+		vscode.commands.executeCommand('workbench.action.files.save').then(function() {
+			copyFile(filePath);
+		});
+	});
+
+	context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
